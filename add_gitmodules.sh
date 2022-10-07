29ceca72c3fb8e77b6146e473c2a72a704fa0bbb
chensong <chensongbj25@gmail.com>
2022-10-08 07:10:44 +0800

    [git submodule add https://github.com/chensongpoixs/cjpeg_encode_decoder.git jpeg_encode_decoder ]

--- add_gitmodules.sh
+++ add_gitmodules.sh
@@ -4,3 +4,6 @@
 
 git submodule add https://github.com/chensongpoixs/caudio_video.git audio_video_example
 git submodule add https://github.com/chensongpoixs/cvideo_codec.git video_codec
+# 
+git submodule add https://github.com/chensongpoixs/cjpeg_encode_decoder.git jpeg_encode_decoder
+
